546e561128dc54965b6f162c3283480d07471dc2
AgataAlves <agataalvesdeandrade@gmail.com>
2025-06-11 14:59:45 -0300

    Add files via upload

---
+++ archecture-site/js/javascript.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector(".navbar");
+
+  function handleScroll() {
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  }
+
+  window.addEventListener("scroll", handleScroll);
+
+  console.log("Archecture site inicializado!");
+});
+
+
+// validação de formulários do Bootstrap
+(() => {
+  'use strict'
+
+  const forms = document.querySelectorAll('.needs-validation')
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
